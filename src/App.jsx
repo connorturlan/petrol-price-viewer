c7d5388b7cde84ceaa6e75ec8640cfd9627ebfbd
Connor <connorturlan@gmail.com>
2023-05-14 11:38:20 +0930

    added initial map

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import GeoJSON from "ol/format/GeoJSON";
+import MapWrapper from "./components/MapWrapper";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	// set intial state
+	const [features, setFeatures] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	// initialization - retrieve GeoJSON features from Mock JSON API get features from mock
+	//  GeoJson API (read from flat .json file in public directory)
+	useEffect(() => {
+		fetch("/mock-geojson-api.json")
+			.then((response) => response.json())
+			.then((fetchedFeatures) => {
+				// parse fetched geojson into OpenLayers features
+				//  use options to convert feature from EPSG:4326 to EPSG:3857
+				const wktOptions = {
+					dataProjection: "EPSG:4326",
+					featureProjection: "EPSG:3857",
+				};
+				const parsedFeatures = new GeoJSON().readFeatures(
+					fetchedFeatures,
+					wktOptions
+				);
+
+				// set features into state (which will be passed into OpenLayers
+				//  map component as props)
+				setFeatures(parsedFeatures);
+			});
+	}, []);
+
+	return (
+		<div className="App">
+			<div className="app-label">
+				<p>React Functional Components with OpenLayers Example</p>
+				<p>
+					Click the map to reveal location coordinate via React State
+				</p>
+			</div>
+
+			<MapWrapper features={features} />
+		</div>
+	);
 }
 
-export default App
+export default App;
