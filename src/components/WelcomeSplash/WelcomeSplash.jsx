9f559d80c7bc2e669def48a16a26784c3c747539
Connor <connorturlan@gmail.com>
2024-10-05 18:31:55 +0930

    extending the welcome splash

--- src/components/WelcomeSplash/WelcomeSplash.jsx
+++ src/components/WelcomeSplash/WelcomeSplash.jsx
@@ -20,16 +20,26 @@ const WelcomeSplash = (props) => {
         >
           <h2 className={styles.WelcomeSplash_Title}>Fuel Tool</h2>
           <p>
-            Hello and welcome to fuel tool! Your handy tool for saving money on
-            your petrol bill
+            Hello and welcome to Fuel Tool! Your handy tool for saving money on
+            your petrol bill.
           </p>
+          <b>
+            This app is currently in development and all features are subject to
+            change without notice. If you have any issues while using the app
+            please reach out to{" "}
+            <a href="mailto://connorturlan@gmail.com?subject=Fuel Tool issue report">
+              connorturlan@gmail.com
+            </a>
+            .
+          </b>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, a
-            sapiente corrupti, explicabo, rerum rem fuga distinctio unde earum
-            recusandae quae quos eos perspiciatis fugiat nobis eligendi ex?
-            Placeat, totam.
+            Fuel Tool is provided as a free service for viewing petrol prices
+            across South Australia. Features such as route price filtering,
+            price alerts, price thresholds, and more, require registration and
+            payment.
           </p>
-          <p>Touch anywhere to hide</p>
+          <p>Thank you for using Fuel Tool!</p>
+          <b>Touch anywhere to hide</b>
         </div>
       </div>
     )
