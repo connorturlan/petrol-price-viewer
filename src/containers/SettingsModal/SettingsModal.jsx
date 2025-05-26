84938ff41e92fedb3c93144786549629b47e9077
Connor <connorturlan@gmail.com>
2025-05-26 11:00:42 +0930

    updating settings modal

--- src/containers/SettingsModal/SettingsModal.jsx
+++ src/containers/SettingsModal/SettingsModal.jsx
@@ -5,6 +5,7 @@ import { UserContext } from "../../contexts/UserContext";
 import { AppContext } from "../../contexts/AppContext";
 import { capitalize, ObjectIsEmpty } from "../../utils/utils";
 import { getCoordinatesOfAddress } from "../../utils/navigation";
+import ToolboxModal from "../ToolboxModal/ToolboxModal";
 
 const SettingsModal = () => {
   // validate that the user is logged in before showing.
@@ -50,138 +51,128 @@ const SettingsModal = () => {
   //
   return (
     <>
-      {visible && (
+      <ToolboxModal
+        summary={
+          <>
+            <img
+              src="settings_24dp_FILL0_wght400_GRAD0_opsz24.svg"
+              className={styles.SettingsModal_Image}
+              alt="Show"
+              srcSet=""
+              title="Show price chart"
+            />
+            <p>Settings</p>
+          </>
+        }
+      >
         <div
-          className={styles.SettingsModal_Container}
-          onClick={() => {
-            setVisible(false);
+          className={styles.SettingsModal_Body}
+          onClick={(e) => {
+            e.stopPropagation();
           }}
         >
-          <div
-            className={styles.SettingsModal_Body}
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-          >
-            <h2 className={styles.SettingsModal_Title}>Settings</h2>
-            <div className={styles.SettingsModal_List}>
-              <h3>Points of Interest</h3>
-              <button
-                className={styles.SetHome}
-                onClick={() => {
-                  setVisible(false);
-                  setClickMode(MODES.ADD_HOME);
-                }}
-                disabled={clickMode == MODES.ADD_HOME}
-              >
-                <img
-                  src="home_pin_24dp_FILL0_wght400_GRAD0_opsz24_dkgreen.svg"
-                  className={styles.SetHome_Image}
-                ></img>
-                <p>Set Home</p>
-              </button>
+          <h2 className={styles.SettingsModal_Title}>Settings</h2>
+          <div className={styles.SettingsModal_List}>
+            <h3>Points of Interest</h3>
+            <button
+              className={styles.SetHome}
+              onClick={() => {
+                setVisible(false);
+                setClickMode(MODES.ADD_HOME);
+              }}
+              disabled={clickMode == MODES.ADD_HOME}
+            >
+              <img
+                src="home_pin_24dp_FILL0_wght400_GRAD0_opsz24_dkgreen.svg"
+                className={styles.SetHome_Image}
+              ></img>
+              <p>Set Home</p>
+            </button>
 
-              <button
-                className={styles.SetHome}
-                onClick={() => {
-                  setVisible(false);
-                  setClickMode(MODES.ADD_WORK);
-                }}
-                disabled={clickMode == MODES.ADD_WORK}
-              >
-                <img
-                  src="person_pin_circle_24dp_255290_FILL0_wght400_GRAD0_opsz24.svg"
-                  className={styles.SetHome_Image}
-                ></img>
-                <p>Set Work</p>
-              </button>
+            <button
+              className={styles.SetHome}
+              onClick={() => {
+                setVisible(false);
+                setClickMode(MODES.ADD_WORK);
+              }}
+              disabled={clickMode == MODES.ADD_WORK}
+            >
+              <img
+                src="person_pin_circle_24dp_255290_FILL0_wght400_GRAD0_opsz24.svg"
+                className={styles.SetHome_Image}
+              ></img>
+              <p>Set Work</p>
+            </button>
 
+            {Object.keys(POI).map((poi) => {
+              if (["home", "work"].includes(poi)) return;
+              return (
+                <button
+                  key={poi}
+                  className={styles.SetHome}
+                  onClick={() => {
+                    removeLocation(poi);
+                  }}
+                >
+                  <img
+                    src="location_on_24dp_8C1A10_FILL0_wght400_GRAD0_opsz24.svg"
+                    className={styles.SetHome_Image}
+                  ></img>
+                  <p>Remove {capitalize(poi)}</p>
+                </button>
+              );
+            })}
+
+            <button
+              className={styles.SetHome}
+              onClick={async () => {
+                const address = window.prompt("Address");
+                if (!address) {
+                  window.alert(`"${name} is not valid."`);
+                  return;
+                }
+                const coord = await lookup(address);
+                if (!coord) {
+                  return;
+                }
+
+                const name = window.prompt("Location name");
+                if (!name) {
+                  window.alert(`"${name} is not valid."`);
+                  return;
+                }
+
+                setCustomLocation(profile, name, coord);
+              }}
+            >
+              <img
+                src="location_on_24dp_8C1A10_FILL0_wght400_GRAD0_opsz24.svg"
+                className={styles.SetHome_Image}
+              ></img>
+              <p>Add Custom Destination</p>
+            </button>
+            <h3>Preferences</h3>
+            <button
+              // className={styles.SetHome}
+              onClick={() => {
+                setDarkMode(!darkMode);
+              }}
+            >
+              <p>DarkMode</p>
+            </button>
+            <h3>Details</h3>
+            <div>
               {Object.keys(POI).map((poi) => {
-                if (["home", "work"].includes(poi)) return;
                 return (
-                  <button
-                    key={poi}
-                    className={styles.SetHome}
-                    onClick={() => {
-                      removeLocation(poi);
-                    }}
-                  >
-                    <img
-                      src="location_on_24dp_8C1A10_FILL0_wght400_GRAD0_opsz24.svg"
-                      className={styles.SetHome_Image}
-                    ></img>
-                    <p>Remove {capitalize(poi)}</p>
-                  </button>
+                  <p key={poi}>
+                    {poi}: {POI[poi].Lat}, {POI[poi].Lng}
+                  </p>
                 );
               })}
-
-              <button
-                className={styles.SetHome}
-                onClick={async () => {
-                  const address = window.prompt("Address");
-                  if (!address) {
-                    window.alert(`"${name} is not valid."`);
-                    return;
-                  }
-                  const coord = await lookup(address);
-                  if (!coord) {
-                    return;
-                  }
-
-                  const name = window.prompt("Location name");
-                  if (!name) {
-                    window.alert(`"${name} is not valid."`);
-                    return;
-                  }
-
-                  setCustomLocation(profile, name, coord);
-                }}
-              >
-                <img
-                  src="location_on_24dp_8C1A10_FILL0_wght400_GRAD0_opsz24.svg"
-                  className={styles.SetHome_Image}
-                ></img>
-                <p>Add Custom Destination</p>
-              </button>
-              <h3>Preferences</h3>
-              <button
-                // className={styles.SetHome}
-                onClick={() => {
-                  setDarkMode(!darkMode);
-                }}
-              >
-                <p>DarkMode</p>
-              </button>
-              <h3>Details</h3>
-              <div>
-                {Object.keys(POI).map((poi) => {
-                  return (
-                    <p key={poi}>
-                      {poi}: {POI[poi].Lat}, {POI[poi].Lng}
-                    </p>
-                  );
-                })}
-              </div>
             </div>
-            <p>Touch anywhere to hide</p>
           </div>
         </div>
-      )}
-      <button
-        className={styles.SettingsModal_Show}
-        onClick={() => {
-          setVisible(true);
-        }}
-      >
-        <img
-          src="settings_24dp_FILL0_wght400_GRAD0_opsz24.svg"
-          className={styles.SettingsModal_Image}
-          alt="Show"
-          srcSet=""
-          title="Show price chart"
-        />
-        <p>Settings</p>
-      </button>
+      </ToolboxModal>
     </>
   );
 };
