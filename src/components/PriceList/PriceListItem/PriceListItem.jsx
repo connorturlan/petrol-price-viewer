fd778a0bbccd9ce2b050de67e1e094e06a3c6472
Connor <connorturlan@gmail.com>
2025-08-17 15:18:25 +0930

    updating petrol list style

--- src/components/PriceList/PriceListItem/PriceListItem.jsx
+++ src/components/PriceList/PriceListItem/PriceListItem.jsx
@@ -4,7 +4,7 @@ import brands from "../../../assets/brands-indev.json";
 function PriceListItem({ name, price, image, showDetails }) {
   return (
     <div className={styles.PriceListItem} onClick={showDetails}>
-      <img src={image || "red-pin.svg"} width={20} height={27} />
+      <img src={image || "red-pin.svg"} />
       <p>{name}</p>
       <p>{price}</p>
     </div>
