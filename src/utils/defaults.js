5ebd0e76286ee2a8ff1ad1ca1d9f96518b55ac97
Connor <connorturlan@gmail.com>
2025-11-27 21:45:42 +1030

    redirect to the new api

--- src/utils/defaults.js
+++ src/utils/defaults.js
@@ -1,8 +1,9 @@
 export const ENDPOINT =
   import.meta.env.VITE_LOCAL == "TRUE" ||
   import.meta.env.VITE_LOCAL_API == "TRUE"
-    ? "http://localhost:3000"
-    : "https://ad8rhw1x2h.execute-api.ap-southeast-2.amazonaws.com/Prod";
+    ? "https://api.fueltool.com.au"
+    : "https://api.fueltool.com.au";
+// : "https://ad8rhw1x2h.execute-api.ap-southeast-2.amazonaws.com/Prod";
 
 // export const PROJECTION = "EPSG:4326";
 export const PROJECTION = "EPSG:3857";
